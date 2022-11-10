f23d6b79ec2c436bc7c4d11605db37ca1342f0f6
Oscar Davies <oscar.william.davies@outlook.com>
2022-11-10 14:32:07 +0000

    Working Image Scraper

---
+++ TinderLikes/Program.cs
@@ -0,0 +1,78 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Text.Json;
+
+var tokenFile = "token.txt";
+
+if (!File.Exists(tokenFile))
+{
+    File.Create(tokenFile);
+    Console.WriteLine("Please Paste Your Token Into " + tokenFile);
+    return;
+}
+
+string token = File.ReadAllText(tokenFile);
+
+if (token.Length == 0)
+{
+    Console.WriteLine("Please Correct Your Token In " + tokenFile);
+    return;
+}
+
+if (!Directory.Exists("likes")) Directory.CreateDirectory("likes");
+
+using (var webClient = new WebClient())
+{
+    using (var httpClient = new HttpClient())
+    {
+        while (true)
+        {
+            using (var request = new HttpRequestMessage(new HttpMethod("GET"), "https://api.gotinder.com/v2/fast-match/teasers?locale=en-GB"))
+            {
+                Console.WriteLine("Finding Likes");
+
+                request.Headers.TryAddWithoutValidation("platform", "android");
+                request.Headers.TryAddWithoutValidation("x-auth-token", token);
+
+                var response = await httpClient.SendAsync(request);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Your Token Is Likely Invalid, Please Update It In " + tokenFile);
+                    return;
+                }
+
+                var stream = response.Content.ReadAsStringAsync();
+                stream.Wait();
+
+                var body = stream.Result;
+
+                var json = JsonSerializer.Deserialize<JsonElement>(body);
+
+                var results = json.GetProperty("data").GetProperty("results").EnumerateArray();
+
+                foreach (var u in results)
+                {
+                    var user = u.GetProperty("user");
+
+                    var userId = user.GetProperty("_id").GetString();
+
+                    var photos = user.GetProperty("photos").EnumerateArray();
+
+                    var photoUrl = photos.First().GetProperty("url").GetString();
+
+                    var fileName = userId + ".jpeg";
+                    var filePath = "./likes/" + fileName;
+
+                    if (!File.Exists(filePath))
+                    {
+                        Console.WriteLine("Downloading " + fileName);
+                        await webClient.DownloadFileTaskAsync(new Uri(photoUrl), filePath);
+                    }
+                }
+            }
+            Console.WriteLine("Waiting Before Repeating");
+            Thread.Sleep(60000);
+        }
+    }
+}
